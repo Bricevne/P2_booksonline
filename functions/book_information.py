0913962d59146d59e9254f3e190c28d81e678e9e
Brice Venayre <brice.venayre@gmail.com>
2022-02-23 19:08:44 +0100

    Add functions package

---
+++ functions/book_information.py
@@ -0,0 +1,36 @@
+"""
+Functions to get all the book's information
+"""
+
+from urllib import request as req
+
+# def get_book_url():
+    
+def get_title(soup_object):
+    return soup_object.find("h1").text
+
+def get_universal_product_code(product_information_table):
+    return product_information_table[0].text
+
+def get_price_including_tax(product_information_table):
+    return product_information_table[3].text
+
+def get_price_excluding_tax(product_information_table):
+    return product_information_table[2].text
+
+def get_number_available(product_information_table):
+    number = product_information_table[5].text
+    return "".join([character for character in number if character.isdigit()])
+
+def get_product_description(soup_object):
+    return soup_object.find("article", {"class" : "product_page"}).findAll("p")[3].text
+
+def get_category(soup_object):
+    return soup_object.find("ul", {"class" : "breadcrumb"}).findAll("a")[2].text
+
+def get_review_rating(soup_object):
+    return soup_object.find("p", {"class" : "star-rating"})["class"][1]
+
+def get_image_url(soup_object):
+    image_url = soup_object.find("img")["src"]
+    return image_url.replace("../..", "https://books.toscrape.com/")
--- main.py
+++ main.py
@@ -1,37 +1,41 @@
 import requests
 from bs4 import BeautifulSoup
-from urllib import request
+from urllib import request as req
+from functions.book_information import *
 
+page_url = "https://books.toscrape.com/catalogue/category/books/travel_2/index.html"
 
-url = "http://books.toscrape.com/catalogue/its-only-the-himalayas_981/index.html"
-
-response = requests.get(url)
+response = requests.get(page_url)
 response.encoding = 'utf8'
 
 if response.ok:
     soup = BeautifulSoup(response.text, features="html.parser")
 
-    product_details = {}
-    current_url=request.Request(url)
-    product_details["product_page_url"] = current_url.get_full_url()
-    product_details["title"] = soup.find("h1").text
-
-    product_table = soup.findAll("td")
-    product_details["universal_product_code"] = product_table[0].text
-    title = soup.find("div", {"class" : "product_main"}).text
-    product_details["price_including_tax"] = product_table[3].text
-    product_details["price_excluding_tax"] = product_table[2].text
-
-    number_available_text = product_table[5].text
-    product_details["number_available"] = "".join([character for character in number_available_text if character.isdigit()])
+    books_page = soup.findAll("h3")
+    for i in range(len(books_page)):
+        books_page[i] = books_page[i].find("a")["href"].replace("../../..", "https://books.toscrape.com/catalogue")
     
-    product_details["product_description"] = soup.find("article", {"class" : "product_page"}).findAll("p")[3].text
-        
-    product_details["category"] = soup.find("ul", {"class" : "breadcrumb"}).findAll("a")[2].text
+    for books_nb in range(len(books_page)):
+        response = requests.get(books_page[books_nb])
+        response.encoding = 'utf8'
 
-    product_details["review_rating"] = soup.find("p", {"class" : "star-rating"})["class"][1]
+        if response.ok:
+            soup = BeautifulSoup(response.text, features="html.parser")
 
-    image_url = soup.find("img")["src"]
-    product_details["image_url"] = image_url.replace("../..", "https://books.toscrape.com/")
+            product_details = {}
 
-    print(product_details)
+            product_details["product_page_url"] = books_page[books_nb]
+            product_details["title"] = get_title(soup)
+
+            product_table = soup.findAll("td")
+
+            product_details["universal_product_code"] = get_universal_product_code(product_table)
+            product_details["price_including_tax"] = get_price_including_tax(product_table)
+            product_details["price_excluding_tax"] = get_price_excluding_tax(product_table)
+            product_details["number_available"] = get_number_available(product_table)
+            product_details["product_description"] = get_product_description(soup)
+            product_details["category"] = get_category(soup)
+            product_details["review_rating"] = get_review_rating(soup)
+            product_details["image_url"] = get_image_url(soup)
+
+            print(product_details)
