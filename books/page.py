4f03af5c4790bf9abecdcb974b2e1b94d962b3a6
Brice Venayre <brice.venayre@gmail.com>
2022-03-05 15:55:13 +0100

    add module page

---
+++ books/page.py
@@ -0,0 +1,25 @@
+"""
+Books' pages control.
+"""
+
+import requests
+from bs4 import BeautifulSoup
+
+
+
+def format_page_url(url: str) -> str:
+    response = requests.get(url)
+    response.encoding = 'utf8'
+    if response.ok:
+        soup = BeautifulSoup(response.text, features="html.parser")
+        number_of_article = int(soup.find("form", {"class" : "form-horizontal"}).find("strong").text)
+        if number_of_article > 21:
+            return url.replace("index", "page-1")
+    return url
+
+
+def get_next_page(page_number: int, url: str) -> list:
+    next_page_number = page_number + 1
+    url = url.replace("page-" + str(page_number), "page-" + str(next_page_number))
+    page_number = next_page_number
+    return url
