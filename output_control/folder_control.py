913a2c4ebe7996d93e487d2b080335c76e922ba6
Brice Venayre <brice.venayre@gmail.com>
2022-03-11 17:04:18 +0100

    update folder_control.py - delete_output_folders

--- output_control/folder_control.py
+++ output_control/folder_control.py
@@ -2,14 +2,18 @@
 Create the necessary output folders.
 """
 
-import os
+from os import mkdir
+from shutil import rmtree
 
-def create_output_folders(categories: dict):
+def create_output_folders(path: str, categories: dict):
     """Create folders for csv and jpg files."""
-    os.mkdir("products")
-    os.mkdir("products/images")
-    os.mkdir("products/information")
+    mkdir(path)
+    mkdir(f"{path}/images")
+    mkdir(f"{path}/information")
     for key in categories.keys():
-        os.mkdir(f"products/images/{key}")
+        mkdir(f"products/images/{key}")
 
+def delete_ouput_folders(path: str):
+    """Delete all output folders."""
+    rmtree(path)
 
